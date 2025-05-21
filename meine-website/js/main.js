1d26217a2268934eedc7a34411d1f84a9ec992f7
itsliande <socialmedia@itslian.de>
2025-05-21 06:15:44 +0000

    Erstelle die Grundstruktur der Website mit HTML, CSS und JavaScript

---
+++ meine-website/js/main.js
@@ -0,0 +1,5 @@
+// main.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Die Website ist bereit!');
+    // Hier können weitere Funktionen zur Interaktivität hinzugefügt werden.
+});
