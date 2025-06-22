1b6d2abbf29468c837c812840273d61007f63761
itsliande <socialmedia@itslian.de>
2025-06-22 17:24:36 +0000

    Aktualisiere Firebase-Konfiguration mit echten Werten für das Rosenrausch Admin Panel

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,12 +7,12 @@ import { getStorage } from 'firebase/storage';
 // Firebase Konfiguration
 // WICHTIG: Diese Werte müssen durch echte Firebase-Konfiguration ersetzt werden
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "API_KEY_RAUSHI",
+  authDomain: "rosenrasch.firebaseapp.com",
+  projectId: "rosenrasch",
+  storageBucket: "rosenrasch.firebasestorage.app",
+  messagingSenderId: "238261942819",
+  appId: "1:238261942819:web:3294f6c8031303f423cf96"
 };
 
 // Firebase initialisieren
