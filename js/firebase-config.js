f017e8690a973b5cee368e0231b318bba924b10c
itsliande <socialmedia@itslian.de>
2025-06-22 18:16:34 +0000

    Aktualisiere Firebase-Konfiguration zur Verwendung von Umgebungsvariablen für den API-Schlüssel. Füge Beispiel für .env-Datei hinzu und erweitere .gitignore um Umgebungsvariablen.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,7 +7,7 @@ import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-s
 // Firebase Konfiguration
 // WICHTIG: Diese Werte müssen durch echte Firebase-Konfiguration ersetzt werden
 const firebaseConfig = {
-  apiKey: "API_KEY_RAUSHI",
+  apiKey: process.env.API_KEY_RAUSHI,
   authDomain: "rosenrasch.firebaseapp.com",
   projectId: "rosenrasch",
   storageBucket: "rosenrasch.firebasestorage.app",
